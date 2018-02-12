b43fab98c2748264a35f4b853b0e464948ce1168
Sihe Zhang <liubing1@msu.edu>
2018-02-12 15:38:25 -0500

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+var buttonElement1 = document.getElementById('First-button');
+var buttonElement2 = document.getElementById('Second-button');
+var buttonElement3 = document.getElementById('Third-button');
+var aud1 = document.getElementById("myAudio1");
+var aud2 = document.getElementById("myAudio2");
+var aud3 = document.getElementById("myAudio3");
+
+function playAud() {
+	aud.play();
+}
+
+buttonElement1.addEventListener('click', playAud(aud1));
+buttonElement2.addEventListener('click', playAud(aud2));
+buttonElement3.addEventListener('click', playAud(aud3));
