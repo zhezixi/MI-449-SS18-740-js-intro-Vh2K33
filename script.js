18c52a60e925f046a5244c99cc8a160d06b4017c
Sihe Zhang <liubing1@msu.edu>
2018-03-07 08:38:49 -0500

    ids, audio elements and identation fixed

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ var buttonElement2 = document.getElementById('Second-button');
 var buttonElement3 = document.getElementById('Third-button');
 
 buttonElement1.addEventListener("click", function (){
-document.getElementById("myAudio1").play()
+  document.getElementById("tom").play()
 })
 buttonElement1.addEventListener("mouseover",function(){
-  document.getElementById("myAudio1").play()
+  document.getElementById("tom").play()
 })
 
 buttonElement2.addEventListener("click", function(){
-  document.getElementById("myAudio2").play()
+  document.getElementById("snare").play()
 })
 buttonElement2.addEventListener("mouseover",function(){
-  document.getElementById("myAudio2").play()
+  document.getElementById("snare").play()
 })
 
 buttonElement3.addEventListener("click", function(){
-  document.getElementById("myAudio3").play()
+  document.getElementById("ride").play()
 })
 buttonElement3.addEventListener("mouseover",function(){
-  document.getElementById("myAudio3").play()
+  document.getElementById("ride").play()
 })
