0fdb7ba17201eeeff68d5d1d4db982300a4b8a96
Sihe Zhang <liubing1@msu.edu>
2018-03-08 01:25:06 -0500

    namings fixed

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // JavaScript Document
-var buttonElement1 = document.getElementById('First-button');
-var buttonElement2 = document.getElementById('Second-button');
-var buttonElement3 = document.getElementById('Third-button');
+var tomButton = document.getElementById('Tom-Button');
+var snareButton = document.getElementById('Snare-Button');
+var rideButton = document.getElementById('Ride-Button');
 
-buttonElement1.addEventListener("click", function (){
+tomButton.addEventListener("click", function (){
   document.getElementById("tom").play()
 })
-buttonElement1.addEventListener("mouseover",function(){
+tomButton.addEventListener("mouseover",function(){
   document.getElementById("tom").play()
 })
 
-buttonElement2.addEventListener("click", function(){
+snareButton.addEventListener("click", function(){
   document.getElementById("snare").play()
 })
-buttonElement2.addEventListener("mouseover",function(){
+snareButton.addEventListener("mouseover",function(){
   document.getElementById("snare").play()
 })
 
-buttonElement3.addEventListener("click", function(){
+rideButton.addEventListener("click", function(){
   document.getElementById("ride").play()
 })
-buttonElement3.addEventListener("mouseover",function(){
+rideButton.addEventListener("mouseover",function(){
   document.getElementById("ride").play()
 })
